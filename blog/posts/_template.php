d4b3ebec50c1aaaaaee3c641bc46b33783621d9e
Vincent Yang <yang.vincent.h@icloud.com>
2026-02-08 20:12:21 +0800

    Update Buy Me a Coffee link to remove 'www' for consistency

--- blog/posts/_template.php
+++ blog/posts/_template.php
@@ -77,7 +77,7 @@ $formattedDate = date('F j, Y', strtotime($blogDate));
                     <a href="/index.php" class="back-link" data-i18n="blog.backToList">← Back to Blog List</a>
                 </div>
                 <div class="blog-actions-coffee">
-                    <a href="https://www.buymeacoffee.com/vincentyang" class="buy-coffee-link"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=vincentyang&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
+                    <a href="https://buymeacoffee.com/vincentyang" class="buy-coffee-link"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=vincentyang&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
                 </div>
             </div>
             <h1><?php echo htmlspecialchars($blogTitle); ?></h1>
@@ -120,7 +120,7 @@ $formattedDate = date('F j, Y', strtotime($blogDate));
                     <a href="/index.php" class="back-link" data-i18n="blog.backToList">← Back to Blog List</a>
                 </div>
                 <div class="blog-actions-coffee">
-                    <a href="https://www.buymeacoffee.com/vincentyang" class="buy-coffee-link"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=vincentyang&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
+                    <a href="https://buymeacoffee.com/vincentyang" class="buy-coffee-link"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=vincentyang&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
                 </div>
             </div>
         </footer>
